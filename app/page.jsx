8f99c7ab54a2b330854c57589c018dac6032c357
Aftiyan <aftiyangamer45@gmail.com>
2024-01-19 21:53:28 +0700

    first pake necxt yeayyy

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="min-h-[100dvh] flex justify-center items-center">
+        <p className="font-semibold text-2xl tracking-tight">Mira sumpah lucu bangettt</p>
+    </div>
+  )
+}
+
+export default Home;
